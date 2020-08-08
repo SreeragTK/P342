5c2889c7b6049382dd6567e35ecec4cd22785106
SreeragTK <42139412+SreeragTK@users.noreply.github.com>
2020-08-08 22:03:34 +0530

    Update q2.cpp

--- Assignment 1/C++/q2.cpp
+++ Assignment 1/C++/q2.cpp
@@ -1,32 +1,30 @@
 #include<iostream>
 using namespace std;
 int main(){
-double n;
-cout<<"Choose a number of your choice to compute factorial:\n";
-cin>>n;
-double x;
-double m;
-x=n;
-m=n;
-if(n==0){
-    cout<<"The value of 0! is 1";
-        }
-else{
-    if(n>0){
-        if(n==1){
-            cout<<"The value of 1! is 1";
-            }
-        else{
-            while(n>1){
+        double n;
+        cout<<"Choose a number of your choice to compute factorial:\n";
+        cin>>n;
+        double x;
+        double m;
+        x=n;
+        m=n;
+        if(n==0){
+            cout<<"The value of 0! is 1";
+                }
+        if(n>0){
+            if(n==1){
+                cout<<"The value of 1! is 1";
+                    }
+            else{
+                while(n>1){
                 x=x*(n-1);
                 n--;
-                }
-            cout<<"The value of "<<m<<"! is "<<x;
-            }
-        }
-    else{
-        cout<<"The number you entered is negative!,please enter any positive number(zero included)";
-        }
+                   }
+                cout<<"The value of "<<m<<"! is "<<x;
+                 }
+               }
+        else{
+            cout<<"The number you entered is negative!,please enter any positive number(zero included)";
             }
 return 0;
 }
