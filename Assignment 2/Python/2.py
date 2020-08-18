12a50b15c96aa14aacbe8c7c8334af3f4878331a
SreeragTK <42139412+SreeragTK@users.noreply.github.com>
2020-08-18 12:43:53 +0530

    Update 2.py

--- Assignment 2/Python/2.py
+++ Assignment 2/Python/2.py
@@ -14,3 +14,8 @@ while i<3:
     dot_product=dot_product+(a[i]*b[i])
     i+=1
 print("The dot product of these to vectors is:",dot_product)
+#The output is:
+#The vector B is:[1,3,6]
+#The vector B is:[2,7,9]
+#The sum of these two vectors is(A+B):[3,10,15]
+#Dot product of these two vectors is(A.B):77
